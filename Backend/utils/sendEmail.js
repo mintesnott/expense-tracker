fc1dffea806543eaa98941fae605d7a72e70f7a3
Melos <mintesnotkebede6@gmail.com>
2026-08-12 01:50:45 +0300

    Update Gmail SMTP configuration

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -4,8 +4,8 @@ const sendEmail = async ({ to, subject, html }) => {
     console.log("EMAIL: creating transporter");
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-        port: Number(process.env.EMAIL_PORT || 465),
-        secure: true,
+        port: Number(process.env.EMAIL_PORT || 587),
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
